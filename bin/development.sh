188bf5694372c8d194265be2db2439a4042526eb
Evan Summers <evan.summers@gmail.com>
2018-08-11 21:07:19 +0100

    initial

--- bin/development.sh
+++ bin/development.sh
@@ -1,12 +1,33 @@
 
   redis-cli keys 'test:remake:*' | xargs redis-cli del
 
-  curl -d '{"name":"evan"}' -H "Content-Type: application/json" localhost:8888/post; echo
+  ( 
+    curl -d '{"name":"evan"}' -H "Content-Type: application/json" localhost:8888/post 
+    echo
+  ) &
+  ( 
+    curl -d '{"name":"evan"}' -H "Content-Type: application/json" localhost:8888/post 
+    echo
+  ) &
+
+  sleep 1
 
   redis-cli keys '*' 
 
-  redis-cli hgetall test:remake:hreq:1:h
+  redis-cli hkeys test:remake:hreq:1:h
 
   redis-cli hget test:remake:hreq:1:h body | jq '.'
+
+  sleep 1 
+
+  redis-cli hset test:remake:hres:1:h status 200
+  redis-cli hset test:remake:hres:1:h body 'hello1'
+  redis-cli lpush test:remake:hres:1:q ''
+
+  redis-cli hset test:remake:hres:2:h status 200
+  redis-cli hset test:remake:hres:2:h body 'hello2'
+  redis-cli lpush test:remake:hres:2:q ''
+
+  sleep 1
 
 
