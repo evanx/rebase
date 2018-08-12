d4fb709a5d89bb752a758f58b8e83ee21432222d
Evan Summers <evan.summers@gmail.com>
2018-08-12 13:00:41 +0100

    initial

--- bin/development.sh
+++ bin/development.sh
@@ -1,33 +1,48 @@
 
-  redis-cli keys 'test:remake:*' | xargs redis-cli del
+redis1() {
+  result=`redis-cli $*`
+  >&2 echo "$*" 
+  if [ "$result" -ne 1 ] 
+  then
+    >&2 echo "result: $result"
+  fi
+}
+
+redise() {
+  >&2 echo "$*"
+  redis-cli $*
+}
+
+  redis-cli keys 'mk:*' | xargs redis-cli del
 
   ( 
     curl -d '{"name":"evan"}' -H "Content-Type: application/json" localhost:8888/post 
-    echo
   ) &
+  sleep .1
   ( 
     curl -d '{"name":"evan"}' -H "Content-Type: application/json" localhost:8888/post 
-    echo
   ) &
 
-  sleep 1
+  sleep .1
 
   redis-cli keys '*' 
 
-  redis-cli hkeys test:remake:hreq:1:h
+  redis-cli hkeys mk:hreq:1:h
 
-  redis-cli hget test:remake:hreq:1:h body | jq '.'
+  redis-cli hget mk:hreq:1:h body | jq '.'
 
-  sleep 1 
+  sleep .1 
+ 
+  redise brpoplpush mk:hreq:q mk:hreq:p:q 1
+  redis1 hset mk:hres:1:h status 200
+  redis1 hset mk:hres:1:h body 'hello1'
+  redis1 publish mk:hres:ch 1
 
-  redis-cli hset test:remake:hres:1:h status 200
-  redis-cli hset test:remake:hres:1:h body 'hello1'
-  redis-cli lpush test:remake:hres:1:q ''
+  redise brpoplpush mk:hreq:q mk:hreq:p:q 1
+  redis1 hset mk:hres:2:h status 200
+  redis1 hset mk:hres:2:h body 'hello2'
+  redis1 publish mk:hres:ch 2
 
-  redis-cli hset test:remake:hres:2:h status 200
-  redis-cli hset test:remake:hres:2:h body 'hello2'
-  redis-cli lpush test:remake:hres:2:q ''
-
-  sleep 1
+  sleep 3
 
 
