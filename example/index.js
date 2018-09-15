02d4563ca9a058f00853d85dfc05cea048f32cba
Evan Summers <evan.summers@gmail.com>
2018-09-15 19:20:52 +0100

    indexer

--- example/index.js
+++ example/index.js
@@ -1,60 +1,65 @@
 const httpHandlers = require('../lib/handlers/index.js')
 
 module.exports = {
-    spec: {
-        type: 'http-server',
-        version: 1,
-        redis: {
-            prefix: 'mk:',
-        },
-        responseTimeoutSeconds: 3,
-        popTimeoutSeconds: 4,
-    },
-    services: [
-        {
-            id: 'users'
-        }
-    ],
-    routes: [
-        {
-            path: '/allo',
-            method: 'get',
-            handler({req}) {
-                return {
-                    status: 200,
-                    contentType: 'application/json',
-                    body: {
-                        path: req.path
-                    }
-                }
+   spec: {
+      type: 'http-server',
+      version: 1,
+      redis: {
+         prefix: 'rb:'
+      },
+      responseTimeoutSeconds: 6,
+      popTimeoutSeconds: 6
+   },
+   tables: {
+      users: {
+         indexes: {
+            email: {
+               unique: true,
+               fields: ['email']
             }
-        },
-        {
-            path: '/users/:id',
-            method: 'get',
-            handler: httpHandlers.get({ table: 'user' })
-        },
-        {
-            path: '/users',
-            method: 'post',
-            handler: httpHandlers.create({ table: 'user' })
-        },
-    ],
-    async router(ctx) {
-        const { spec, logger, req } = ctx
-        if (req.path === '/hello') {
+         }
+      }
+   },
+   routes: [
+      {
+         path: '/allo',
+         method: 'get',
+         handler({ req }) {
             return {
-                status: 200,
-                contentType: 'application/json',
-                body: {
-                    message: 'router',
-                    spec
-                }
+               status: 200,
+               contentType: 'application/json',
+               body: {
+                  path: req.path
+               }
             }
-        }
-        return require('./handlers/home.js')(ctx)
-    },
-    shutdown({ spec, exit, error }) {
-        exit()
-    }
+         }
+      },
+      {
+         path: '/users/:id',
+         method: 'get',
+         handler: httpHandlers.get({ table: 'user' })
+      },
+      {
+         path: '/users',
+         method: 'post',
+         handler: httpHandlers.create({ table: 'user' })
+      }
+   ],
+   async router(ctx) {
+      const { spec, logger, req } = ctx
+      if (req.path === '/hello') {
+         return {
+            status: 200,
+            contentType: 'application/json',
+            body: {
+               message: 'router',
+               spec
+            }
+         }
+      }
+      return require('./handlers/home.js')(ctx)
+   },
+   shutdown({ spec, exit, error }) {
+      exit()
+   }
 }
